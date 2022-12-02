5ca5523c284917cd34b5ef28dd8a5ab0200fb5c2
Joshua Ejike <joshuaejike4221@gmail.com>
2022-12-02 12:10:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { useState } from "react";
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
 
 function App() {
+  const [sta, setSta] = useState("");
+  const handleChange = (e, editor) => {
+    const data = editor.getData();
+    setSta(data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CKEditor
+        editor={ClassicEditor}
+        data={sta}
+        onChange={handleChange}
+        config={{
+          plugins: [Paragraph, Bold, Italic, Essentials],
+          toolbar: ["bold", "italic"],
+        }}
+      />
     </div>
   );
 }
